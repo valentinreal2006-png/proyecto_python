d10104dfdb39c7924f4e01c3f5daabb0418f769d
valentinreal2006-png <pitureal2006@gmail.com>
2025-11-18 09:35:56 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+from paquete_1.modulo_1 import clientes
+from paquete_1.modulo_2 import llamando_modulo_2
+
+cliente_1 = clientes("Valentin","Real","123456")
+
+print(cliente_1)
+
+llamando_modulo_2()
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+    name="clientes",
+    version="17.0.1",
+    packages=["paquete_1py"],
+    install_requires=[],
+    author="Valentin",
+    author_email="pitureal2006@gmail.com",
+    description="Un paquete de ejemplo para demostrar la estructura de paquetes en Python", 
+)
